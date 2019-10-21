2cd75591dafed5283870257c61f01937e263e6fd
JEONGSUJI <wtw9611@gmail.com>
2019-10-21 17:52:30 +0900

    javascript 26 챕터 forEach 예제

---
+++ JavaScript/preview/javascript26_forEach.js
@@ -0,0 +1,25 @@
+const Numbers = (function() {
+  function numbers(){
+    this.numberArray = [];
+  }
+  numbers.prototype.multiply = function (arr) {
+    // 1번방법 const that = this;
+    arr.forEach(function (item) {
+      // 일반 함수로 호출되는 콜백 함수 내부의 this는 전역 객체를 가리킨다.
+      // TypeError: Cannot read property 'numberArray' of undefined
+
+      // 1번방법 that.numberArray.push(item * item);
+      // 3번방법
+      this.numberArray.push(item * item);
+
+    // 2번 방법 }.bind(this));
+    
+    // 3번방법
+    }, this);
+  };
+  return Numbers;
+}());
+
+const numbers = new Numbers();
+numbers.multiply([1, 2, 3]);
+console.log(numbers.numberArray);
